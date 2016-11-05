cf966ea2f7e4fee8c39e47f066834c5f3c56a08c
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-11-05 04:01:28 +0000

    OneWire and sensor setup

--- esp8266.ino
+++ esp8266.ino
@@ -1,6 +1,6 @@
 #include "MyWifi.h"
-
-ESP8266WebServer server(80); // Define port 80 for the web server port
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define WARNING_PIN D0  // Led pin number
 #define SENSOR_PIN A0   // Sensor pin number
@@ -9,17 +9,36 @@ ESP8266WebServer server(80); // Define port 80 for the web server port
 char* WIFI_SSID = "ieeehotspot";
 char* WIFI_PASSWORD = "Jk638td9og35";
 
+int TEMP_MAX = 22;
+int TEMP_MIN = 30;
+
+ESP8266WebServer server(80);  // Define port 80 for the web server port
+
+OneWire oneWire(WARNING_PIN); // Setup a oneWire instance to communicate with any OneWire devices
+
+DallasTemperature sensors(&oneWire);  // Pass our oneWire reference to Dallas Temperature.
+
 void respond() {
+  int temp;
+  sensors.requestTemperatures(); // Send the command to get temperatures
   char response[50];  // variable to hold the data to send
-  sprintf(response, "%d", analogRead(SENSOR_PIN));  // Convert data to string (char*)
+  sprintf(response, "%d", sensors.getTempCByIndex(0));  // Convert data to string (char*)
+  
   server.send(200, "text/plain", response); // Send data as response
+  
+  temp = sensors.getTempCByIndex(0);        // Gets the current temperature
+  if (temp > TEMP_MAX || temp < TEMP_MIN) { //Checks if the temperature is within the defined limits
+    digitalWrite(WARNING_PIN, HIGH);  // If the temperature is out of limits lights up the warning led
+  } else {
+    digitalWrite(WARNING_PIN, LOW);   // If the temperature is within the limits turns off the warning led
+  }
 }
 
 void setup() {
   Serial.begin(115200); // Initializes serial with baud rate as a parameter
   connectWifi(WIFI_SSID, WIFI_PASSWORD); // Tries to connect to a wifi network with credentials as parameters
   Serial.println("");
-  Serial.println(WiFi.localIP()); // Prints internal IP on serial TODO: Make it print on the LCD
+  Serial.println(WiFi.localIP());   // Prints internal IP on serial TODO: Make it print on the LCD
   pinMode(SENSOR_PIN, INPUT);       // Defines sensor pin as being an input
   pinMode(WARNING_PIN, OUTPUT);     // Defines sensor pin as being an input
   digitalWrite(WARNING_PIN, LOW);   // Set the starting state of the led as turned off
