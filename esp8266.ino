97f59b14e6a98e5bb16e7223370fdb819b95fe51
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-11-08 08:36:32 +0000

    Variable name correction

--- esp8266.ino
+++ esp8266.ino
@@ -5,7 +5,7 @@
 #define UNIQUE_ID 00001 // Number to differenciate measure points
 
 #define WARNING_PIN D0  // Led pin number
-#define SENSOR_PIN A0   // Sensor pin number
+#define ONE_WIRE_BUS A0   // Sensor pin number
 
 // WiFi
 /*char* WIFI_SSID = "ieeehotspot";
@@ -35,7 +35,7 @@ void setup() {
   connectWifi(WIFI_SSID, WIFI_PASSWORD); // Tries to connect to a wifi network with credentials as parameters
   Serial.println("");
   Serial.println(WiFi.localIP());   // Prints internal IP on serial TODO: Make it print on the LCD
-  pinMode(SENSOR_PIN, INPUT);       // Defines sensor pin as being an input
+  pinMode(ONE_WIRE_BUS, INPUT);       // Defines sensor pin as being an input
   pinMode(WARNING_PIN, OUTPUT);     // Defines sensor pin as being an input
   digitalWrite(WARNING_PIN, LOW);   // Set the starting state of the led as turned off
   server.on("/", respond);          // Listen for HTTP/GET requests to respond appropriately
