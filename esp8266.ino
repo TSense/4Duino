896785011ad362c62f5c377ead63c8d48f3374a7
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2016-12-01 12:48:33 +0000

    Get temperature limits from the request

--- esp8266.ino
+++ esp8266.ino
@@ -5,9 +5,15 @@
 char* ssid = "ieeehotspot";
 char* password = "Jk638td9og35";
 
+int temp=20;
+int tempLow = 1;
+int tempHigh = 50;
+
 ESP8266WebServer server(7568);  // Define port 80 for the web server port
 
 void respond() {
+  tempLow = server.arg("templow");
+  tempHigh = server.arg("temphigh");
   String s = "";
   s += WiFi.macAddress();
   s += ";";
@@ -18,18 +24,16 @@ void respond() {
   server.send(200, "text/html", s); // Send data as response
 }
 
-void configPage(){
+void configPage() {
   server.send(200, "text/html", "TSense Configuration Page");
 }
 
 void setup() {
   Serial.begin(115200); // Initializes serial with baud rate as a parameter
-  int i = 0;
   WiFi.begin(ssid, password); // Sets Wifi credentials
   while (WiFi.status()  !=  WL_CONNECTED) { // Try to connect until it does
-    delay(100);
+    delay(10);
   }
-  Serial.println("");
   Serial.println(WiFi.localIP());   // Prints internal IP on serial TODO: Make it print on the LCD
   server.on("/temp", respond);          // Listen for HTTP/GET requests to respond appropriately
   server.on("/", configPage);
@@ -38,4 +42,9 @@ void setup() {
 
 void loop() {
   server.handleClient();  // Makes sure to reconnect if wifi fails
+  if(temp < tempLow){
+    Serial.println("Temperature is below the limit!");
+  } else if(temp > tempHigh){
+    Serial.println("Temperature is above the limit!");
+  }
 }
