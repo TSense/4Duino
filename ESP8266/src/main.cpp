46fb7297eeda52a9c19209661ad10b4e9666e651
Daniel Silva <daniel_p_silva_97@hotmail.com>
2017-02-28 15:40:57 +0000

    Add UDP support

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -1,11 +1,13 @@
 #include "ESP8266WebServer.h"
 #include "ESP8266WiFi.h"
+#include "WiFiUdp.h"
 #include "SPI.h"
 #include "SparkFunBME280.h"
 #include "Wire.h"
 #include <SoftwareSerial.h>
 
-#include "secrets.h" // comment these lines and uncomment the following
+char ssid[] = "ieeehotspot";        //  your network SSID (name)
+char pass[] = "Jk638td9og35";       // your network password
 
 // Set default configs that will be changed at the first request (as defined in respond())
 float hum = 1;
@@ -14,6 +16,14 @@ float humHigh = 100;
 float temp = 1;
 float tempLow = 0;
 float tempHigh = 100;
+float alt = 1;
+
+
+WiFiUDP Udp;
+unsigned int localUdpPort = 4210;  // local port to listen on
+char incomingPacket[255];  // buffer for incoming packets
+char  replyPacekt[] = "Hi there! Got the message :-)";  // a reply string to send back
+
 
 BME280 tempSensor;
 
@@ -38,14 +48,12 @@ void respond() {
     s += ";";
     s += hum;
     s += ";";
+    s += alt;
     server.send(200, "text/html", s); // Send response as html for library bug purpose
 }
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(BUILTIN_LED, OUTPUT);
-    digitalWrite(BUILTIN_LED, HIGH);
-
+    pinMode(D0, OUTPUT);
     tempSensor.settings.commInterface = I2C_MODE;
     tempSensor.settings.I2CAddress = 0x77;
     tempSensor.settings.runMode = 3; // 0 = Sleep Mode, 1 or 2= Force Mode, 3 = Normal mode
@@ -57,16 +65,27 @@ void setup() {
 
     tempSensor.begin();
 
-    delay(2000);
-
-    WiFi.begin(SSID, PASS); // Sets Wifi credentials
+    delay(1000);
+    Serial.begin(115200);
+    Serial.println();
+    Serial.printf("Connecting to %s ", ssid);
+    WiFi.begin(ssid, pass);
     while (WiFi.status() != WL_CONNECTED) { // Try to connect until it does
+        digitalWrite(D0, LOW);
         delay(100);
+        Serial.print(".");
+        digitalWrite(D0, HIGH);
+        delay(50);
     }
-    digitalWrite(BUILTIN_LED, LOW);
+    Serial.println();
+    Serial.println("Connected! IP: ");
+    Serial.print(WiFi.localIP());
 
     server.on("/", respond); // Listen for HTTP/GET requests to respond appropriately
     server.begin();     // Start web server
+
+     Udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
 }
 
 void loop() {
@@ -74,14 +93,29 @@ void loop() {
 
     temp = tempSensor.readTempC();
     hum = tempSensor.readFloatHumidity();
-    // Cooks response
-    String s = "";
-    s += WiFi.macAddress();
-    s += ";";
-    s += temp;
-    s += ";";
-    s += hum;
-    Serial.print(s);
-    s = "";
-    delay(500);
+    alt = tempSensor.readFloatAltitudeFeet();
+
+    int packetSize = Udp.parsePacket();
+    if (packetSize)
+    {
+        digitalWrite(D0, LOW);
+        // receive incoming UDP packets
+        Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+        int len = Udp.read(incomingPacket, 255);
+        if (len > 0)
+        {
+        incomingPacket[len] = 0;
+        }
+        Serial.printf("UDP packet contents: %s\n", incomingPacket);
+
+        // send back a reply, to the IP address and port we got the packet from
+        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+        Udp.write(replyPacekt);
+        Udp.endPacket();
+        delay(50);
+        digitalWrite(D0, HIGH);
+        
+    }
+
+    delay(100);
 }
